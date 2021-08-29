0c17ca03609a35c5cb2b8b13a611648c13c7649e
farhan-nahid <nahidahmed9933@gmail.com>
2021-08-29 12:40:54 +0600

    basic event added

---
+++ event-common.js
@@ -0,0 +1,36 @@
+// using onclick function
+
+function handleFunction() {
+  const p = document.getElementById("clicking__method");
+  p.innerText = "By Using On Click Function";
+  p.style.color = "green";
+  p.style.padding = "10px";
+  p.style.background = "#00800042";
+  p.style.marginTop = "40px";
+}
+
+// anonymous function
+
+document
+  .getElementById("direct__function")
+  .addEventListener("click", function () {
+    const p = document.getElementById("clicking__method");
+    p.innerText = "By Using Direct Add Event Listener Function";
+    p.style.color = "green";
+    p.style.padding = "10px";
+    p.style.background = "#00800042";
+    p.style.marginTop = "40px";
+  });
+
+// anonymous function
+
+document.getElementById("submit__value").addEventListener("click", function () {
+  let input = document.getElementById("text__input");
+  const p = document.getElementById("clicking__method");
+  p.innerText = input.value;
+  input.value = "";
+  p.style.color = "green";
+  p.style.padding = "10px";
+  p.style.background = "#00800042";
+  p.style.marginTop = "40px";
+});
